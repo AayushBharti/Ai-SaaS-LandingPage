594b61a97e3c19680b7bc822112aaeb751d6123d
Aayush Bharti <aayushbh2002@gmail.com>
2024-04-20 10:31:26 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -9,6 +10,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header/>
         <Hero/>
+        <Benefits/>
       </div>
 
       <ButtonGradient />
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Section from './Section'
+
+const Benefits = () => {
+  return (
+    <Section>Benefits</Section>
+  )
+}
+
+export default Benefits
