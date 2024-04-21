4b51399c7060a07efdebebcc9a2deba10732039b
Aayush Bharti <aayushbh2002@gmail.com>
2024-04-22 00:45:04 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Button from "./components/Button";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -11,6 +12,7 @@ const App = () => {
         <Header/>
         <Hero/>
         <Benefits/>
+        <Collaboration/>
       </div>
 
       <ButtonGradient />
---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Section from './Section'
+
+const Collaboration = () => {
+  return (
+    <Section crosses>Collaboration</Section>
+  )
+}
+
+export default Collaboration
