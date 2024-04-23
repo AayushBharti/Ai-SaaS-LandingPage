581255aba10daa0ff11b3fa1195577d46ec04895
Aayush Bharti <aayushbh2002@gmail.com>
2024-04-23 18:01:06 +0530

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,14 @@
 import Section from "./Section";
 import Heading from "./Heading";
-import { check, service1 } from "../assets";
-import { brainwaveServices } from "../constants";
-import Generating from './Generating'
+import { check, service1, service2, service3 } from "../assets";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import Generating from "./Generating";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -50,10 +56,96 @@ const Services = () => {
               </ul>
             </div>
 
-            <Generating className="absolute right-4 bottom-4
+            <Generating
+              className="absolute right-4 bottom-4
             left-4 border-n-1/10 border lg:left-1/2 lg:right-auto
-            lg:bottom-8 lg:-translate-x-1/2 md:w-[36rem]"/>
+            lg:bottom-8 lg:-translate-x-1/2 md:w-[36rem]"
+            />
           </div>
+
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div
+              className="relative min-h-[39rem] border
+            border-n-1/10 rounded-3xl overflow-hidden"
+            >
+              <div className="absolute inset-0">
+                <img
+                  src={service2}
+                  className="h-full w-full object-cover"
+                  width={630}
+                  height={750}
+                  alt="robot"
+                />
+              </div>
+
+              <div
+                className="absolute inset-0 flex flex-col
+              justify-end p-8 bg-gradient-to-b from-n-8/10
+              to-n-8/90 lg:p-15"
+              >
+                <h4 className="h4 mb-4"> Photo editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Automatically enhance your photos using our AI app&apos;s
+                  photo editing feature. Try it now!
+                </p>
+              </div>
+              <PhotoChatMessage />
+            </div>
+
+            <div
+              className="p-4 bg-n-7 rounded-3xl overflow-hidden
+            lg:min-h-[46rem]"
+            >
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Video generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The world&apos;s most powerful AI photo and video art
+                  generation engine. What will you create?
+                </p>
+
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      key={index}
+                      className={`flex items-center
+                      justify-center rounded-2xl ${
+                        index === 2
+                          ? `w-[3rem] h-[3rem] p-[0.2rem] bg-conic-gradient 
+                            rounded-2xl md:w-[4.5rem] md:h-[4.5rem]`
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      }`}
+                    >
+                      <div
+                        className={`${
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }`}
+                      >
+                        <img src={item} width={24} height={24} alt={item} />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden
+              md:h-[25rem]">
+                <img
+                  src={service3}
+                  className=" w-full h-full object-cover"
+                  width={520}
+                  height={400}
+                  alt="Scary Robot"
+                />
+
+                <VideoChatMessage/>
+                <VideoBar/>
+              </div>
+            </div>
+          </div>
+
+          <Gradient/>
         </div>
       </div>
     </Section>
--- src/components/design/Services.jsx
+++ src/components/design/Services.jsx
@@ -18,7 +18,7 @@ export const Gradient = () => {
 export const PhotoChatMessage = () => {
   return (
     <div className="absolute top-8 right-8 max-w-[17.5rem] py-6 px-8 bg-black rounded-t-xl rounded-bl-xl font-code text-base lg:top-16 lg:right-[8.75rem] lg:max-w-[17.5rem]">
-      Hey Brainwave, enhance this photo
+      Hey StarForge, enhance this photo
       <ChatBubbleWing className="absolute left-full bottom-0" />
     </div>
   );
