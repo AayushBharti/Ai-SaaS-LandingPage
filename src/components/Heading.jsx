4e3e2509891c43e38473a2022699e8e0a39994d4
Aayush Bharti <aayushbh2002@gmail.com>
2024-04-24 15:30:57 +0530

    update

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Tagline from "./Tagline";
 
-const Heading = ({ className, title, text }) => {
+const Heading = ({ className, title, text, tag }) => {
   return (
     <div
       className={`${className} max-w-[50rem] mx-auto 
       mb-12 lg:mb-20 `}
     >
+      {tag && (
+        <Tagline className="mb-4 md:justify-center">
+          Get started with StarForge
+        </Tagline>
+      )}
       {title && <h2 className="h2 text-center">{title}</h2>}
       {text && <p className="bodey-2 mt-4 text-n-4 text-center">{text}</p>}
     </div>
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,39 +1,61 @@
-import { smallSphere, stars } from "../assets"
-import Heading from "./Heading"
-import Section from "./Section"
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
+import Section from "./Section";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
     <Section className="overflow-hidden" id="pricing">
-        <div className="container relative z-2">
-            <div className="hidden relative justify-center 
-            mb-[6.5rem] lg:flex">
-                <img
-                    src={smallSphere}
-                    className="relative z-1"
-                    width={255}
-                    height={255}
-                    alt="Sphere"
-                />
-                <div className="absolute top-1/2 left-1/2 w-[60rem]
-                -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-                    <img
-                        src={stars}
-                        className="w-full"
-                        width={950}
-                        height={400}
-                        alt="Stars"
-                    />
-                </div>
-            </div>
-
-            <Heading 
-                tag="Get started with StarForge"
-                title="Pay once, use forever"
+      <div className="container relative z-2">
+        <div
+          className="hidden relative justify-center 
+            mb-[6.5rem] lg:flex"
+        >
+          <img
+            src={smallSphere}
+            className="relative z-1"
+            width={255}
+            height={255}
+            alt="Sphere"
+          />
+          <div
+            className="absolute top-1/2 left-1/2 w-[60rem]
+                -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+          >
+            <img
+              src={stars}
+              className="w-full"
+              width={950}
+              height={400}
+              alt="Stars"
             />
+          </div>
         </div>
-    </Section>
-  )
-}
 
-export default Pricing
+        <Heading
+          tag="Get started with StarForge"
+          title="Pay once, use forever"
+        />
+
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <a
+            href="/pricing"
+            className="text-xs font-code font-bold tracking-wider
+                uppercase border-b "
+          >
+            See the full details
+          </a>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Pricing;
